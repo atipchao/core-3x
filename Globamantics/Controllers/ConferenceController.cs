c4d76cbe45b0df74904692b13818259fd87db2dd
atipchao <atip.chaogunhar@cookcountyil.gov>
2021-01-10 14:10:43 -0600

    Initial add

---
+++ Globamantics/Controllers/ConferenceController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Globamantics.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Globamantics.Controllers
+{
+    public class ConferenceController : Controller
+    {
+        public ConferenceController(IConferenceService conferenceService)
+        {
+
+        }
+    } 
+}
